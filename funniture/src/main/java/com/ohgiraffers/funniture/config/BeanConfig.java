0211135d9372f29fdf02ff337d9dc544c68c8158
cookiecookiekim <kimkunam06@gmail.com>
2025-01-27 16:44:05 +0900

    :white_check_mark: 더미 문의 데이터 전체 조회하기 연습 / BeanConfig 추가 / product 쪽 주석해야 run 실행됨

---
+++ funniture/src/main/java/com/ohgiraffers/funniture/config/BeanConfig.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.funniture.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.ohgiraffers.funniture") // funniture 하위 다 읽을 수 있게 설정
+public class BeanConfig {
+
+    @Bean // build.gradle에 의존성 넣고 추가 설정 해줘야함
+    public ModelMapper modelMapper () {
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration() // 설정 가져오기
+                // private 필드에 접근하기 위한 설정
+                // 가져온 설정을 다시 바꿔주기
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                // DTO 필드와 Entity 필드 매칭 가능 여부 설정
+                .setFieldMatchingEnabled(true);
+
+        return modelMapper;
+    }
+}
--- funniture/src/main/java/com/ohgiraffers/funniture/product/model/dao/ProductRepository.java
+++ funniture/src/main/java/com/ohgiraffers/funniture/product/model/dao/ProductRepository.java
@@ -3,7 +3,7 @@ package com.ohgiraffers.funniture.product.model.dao;
 import com.ohgiraffers.funniture.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+// 25-01-27 16:41분 / 이쪽도 주석해야 run 됨
 @Repository
 public interface ProductRepository extends JpaRepository<Product, String> {
 }
