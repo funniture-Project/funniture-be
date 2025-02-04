9b389aa6951e7ea13288765be4302a6bfaa2774b
jin <jin200126@gmail.com>
2025-02-04 20:54:45 +0900

    :sparkles: 상품 등록, :recycle: ProductDTO 분리

--- funniture/src/main/java/com/ohgiraffers/funniture/product/model/dao/ProductRepository.java
+++ funniture/src/main/java/com/ohgiraffers/funniture/product/model/dao/ProductRepository.java
@@ -1,13 +1,20 @@
 package com.ohgiraffers.funniture.product.model.dao;
 
+import com.ohgiraffers.funniture.product.entity.ProductDetailEntity;
 import com.ohgiraffers.funniture.product.entity.ProductEntity;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, String> {
-    // category 에서의 category_code 를 비교해야하기 때문에 findByCategory_CategoryCode
-    List<ProductEntity> findByCategory_CategoryCode(int categoryCode);
+
+    @Query(value = "SELECT MAX(product_no) FROM tbl_product", nativeQuery = true)
+    String findMaxNo();
+
+    @Query(value = "SELECT * FROM tbl_product p WHERE p.category_code IN :categoryCode", nativeQuery = true)
+    List<ProductEntity> findByCategoryCodeIn(@Param("categoryCode") List<Integer> categoryCode);
 }
