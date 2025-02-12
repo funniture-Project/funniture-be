de6e675be624baeb347c229250773ae12389fdcf
cookiecookiekim <kimkunam06@gmail.com>
2025-02-12 19:11:57 +0900

    merge 3 : InquiryEntity 수정

--- funniture/src/main/java/com/ohgiraffers/funniture/inquiry/entity/InquiryEntity.java
+++ funniture/src/main/java/com/ohgiraffers/funniture/inquiry/entity/InquiryEntity.java
@@ -1,52 +1,62 @@
 package com.ohgiraffers.funniture.inquiry.entity;
 
-import com.ohgiraffers.funniture.member.entity.MemberEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 // 자신이 필요한걸로 변경해서 하세요!
-@Entity (name = "inquiry")
+@Entity
 @Table(name = "tbl_inquiry")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Setter
 @Builder(toBuilder = true)
 public class InquiryEntity {
 
     @Id
-    @Column
+    @Column (name = "inquiry_no")
     private String inquiryNo;
 
 //    @Column
 //    private String memberId;
-
 // 문의를 작성한 회원 (Member와 ManyToOne 관계)
-    @ManyToOne
-    @JoinColumn(name = "member_id", referencedColumnName = "member_id")
-    private MemberEntity member;
 
-    @Column
+    //    @ManyToOne
+//    @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+//    private MemberEntity member;
+    @Column (name = "member_id")
+    private String memberId;
+
+    @Column (name = "inquiry_content")
     private String inquiryContent;
 
-    @Column
-    private int showStatus;
+//    @Column (name = "show_status")
+//    private int showStatus;
 
-    @Column
-    private int qnaType;
+//    @Column (name = "qna_type")
+//    private int qnaType;
 
-//    private String ownerNo;
+//    @OneToMany (mappedBy = "ProductEntity")
+//    private List<ProductEntity> productNo;
 
-//    @Column
-//    private String productNo;
+    @Column (name = "product_no")
+    private String productNo;
 
-    @ManyToOne
-    @JoinColumn(name = "product_no", referencedColumnName = "product_no")  // Product와 연결
-    private Product product;
+//    @ManyToOne
+//    @JoinColumn(name = "product_no", referencedColumnName = "product_no")  // Product와 연결
+//    private ProductEntity product;
 
-    @Column
+    @Column (name = "qna_write_time")
     private LocalDateTime qnaWriteTime;
 
+    // InquiryEntity에 해당되지 않는 userName(member)와 productName(product)를
+    // 연관관계 형성하지 않고 삽입
+//    @Transient
+//    private String userName;
+//
+//    @Transient
+//    private String productName;
 }
