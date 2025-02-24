2bf3d94102703b297d22af49141120072ae54f29
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2025-02-24 12:18:49 +0900

    :twisted_rightwards_arrows: PointController 소스트리에서 잘못 삭제함 - 정은미:twisted_rightwards_arrows:

--- funniture/src/main/java/com/ohgiraffers/funniture/point/controllers/PointController.java
+++ funniture/src/main/java/com/ohgiraffers/funniture/point/controllers/PointController.java
@@ -1,46 +1,28 @@
 package com.ohgiraffers.funniture.point.controllers;
 
-import com.ohgiraffers.funniture.deliveryaddress.model.dto.DeliveryAddressDTO;
-import com.ohgiraffers.funniture.point.model.dto.PointDTO;
 import com.ohgiraffers.funniture.point.model.service.PointService;
-import com.ohgiraffers.funniture.rental.model.service.RentalService;
 import com.ohgiraffers.funniture.response.ResponseMessage;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.charset.Charset;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Tag(name = "point API")
 @RestController
 @RequestMapping("/api/v1/point")
 @RequiredArgsConstructor
-public class PointControllers {
+public class PointController {
 
     private final PointService pointService;
 
-
-    @Operation(summary = "사용자별 포인트 조회",
-            description = "사용자 마이페이지, 예약 등록 페이지에서 사용",
-            parameters = {
-                    @Parameter(name = "memberId", description = "사용자 ID(필수)")
-            }
-    )
-    @ApiResponses({
-            @ApiResponse(responseCode = "404",description = "회원 정보를 찾을 수 없습니다."),
-            @ApiResponse(responseCode = "200", description = "포인트 조회 성공")
-    })
     @GetMapping("/{memberId}")
     public ResponseEntity<ResponseMessage> findPointByUser(@PathVariable String memberId) {
         // 서비스에서 포인트 조회
@@ -57,8 +39,5 @@ public class PointControllers {
                     .body(new ResponseMessage(404, "회원 정보를 찾을 수 없습니다.", null));
         }
     }
-
-    
-
 }
 
