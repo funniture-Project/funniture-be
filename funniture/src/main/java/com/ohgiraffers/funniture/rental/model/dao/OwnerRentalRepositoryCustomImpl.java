c391bc51a2ca3c3f7da6be20ecfc33dfb53aaffe
EUNMI <dmssl2486@gmail.com>
2025-03-02 20:28:52 +0900

    :art:[PR]2025/03/02 제공자별 조회 API 수정 - 정은미:art:

--- funniture/src/main/java/com/ohgiraffers/funniture/rental/model/dao/OwnerRentalRepositoryCustomImpl.java
+++ funniture/src/main/java/com/ohgiraffers/funniture/rental/model/dao/OwnerRentalRepositoryCustomImpl.java
@@ -1,9 +1,14 @@
 package com.ohgiraffers.funniture.rental.model.dao;
 
+import com.ohgiraffers.funniture.deliveryaddress.entity.QDeliveryAddressEntity;
+import com.ohgiraffers.funniture.member.entity.QMemberEntity;
+import com.ohgiraffers.funniture.member.entity.QOwnerInfoEntity;
 import com.ohgiraffers.funniture.product.entity.QProductEntity;
 import com.ohgiraffers.funniture.product.entity.QRentalOptionInfoEntity;
+import com.ohgiraffers.funniture.rental.entity.QDetailRentalEntity;
 import com.ohgiraffers.funniture.rental.entity.QUserRentalEntity;
 import com.ohgiraffers.funniture.rental.model.dto.OwnerRentalViewDTO;
+import com.ohgiraffers.funniture.rental.model.dto.RentalDetailDTO;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQuery;
@@ -32,13 +37,16 @@ public class OwnerRentalRepositoryCustomImpl implements OwnerRentalRepositoryCus
         LocalDate currentDate = LocalDate.now();  // 현재 날짜를 사용
 
         // User 쪽과 조인이 같아서 재사용!
-        QUserRentalEntity rental = QUserRentalEntity.userRentalEntity;
+        QOwnerInfoEntity ownerInfo = QOwnerInfoEntity.ownerInfoEntity;
+        QRentalOptionInfoEntity rentalOptionInfo = QRentalOptionInfoEntity.rentalOptionInfoEntity;
+        QDeliveryAddressEntity deliveryAddress = QDeliveryAddressEntity.deliveryAddressEntity;
         QProductEntity product = QProductEntity.productEntity;
-        QRentalOptionInfoEntity optionInfo = QRentalOptionInfoEntity.rentalOptionInfoEntity;
+        QDetailRentalEntity rental = QDetailRentalEntity.detailRentalEntity;
+        QMemberEntity member = QMemberEntity.memberEntity;
 
         // 기본적으로 전체 데이터를 조회할 때는 기간 필터를 추가하지 않음
         BooleanBuilder whereCondition = new BooleanBuilder();
-        whereCondition.and(rental.ownerNo.eq(ownerNo));
+        whereCondition.and(rental.ownerInfoEntity.memberId.eq(ownerNo));
 
         // 기간에 맞는 조건을 추가
         if (period != null) {
@@ -75,19 +83,33 @@ public class OwnerRentalRepositoryCustomImpl implements OwnerRentalRepositoryCus
         JPAQuery<OwnerRentalViewDTO> query = jpaQueryFactory
                 .select(Projections.constructor(OwnerRentalViewDTO.class,
                         rental.rentalNo,
+                        rental.orderDate,
+                        rental.rentalNumber,
+                        rental.rentalState,
+                        rental.deliveryMemo,
+                        ownerInfo.storeName,
+                        product.productName,
+                        rentalOptionInfo.rentalPrice,
+                        rentalOptionInfo.rentalTerm,
+                        rentalOptionInfo.asNumber,
+                        deliveryAddress.destinationName,
+                        deliveryAddress.destinationPhone,
+                        deliveryAddress.destinationAddress,
+                        deliveryAddress.receiver,
+                        member.email,
+                        member.userName,
+                        member.phoneNumber,
                         rental.deliverCom,
                         rental.deliveryNo,
-                        rental.rentalNumber,
                         rental.rentalStartDate,
-                        rental.rentalEndDate,
-                        rental.rentalState,
-                        product.productName,
-                        optionInfo.rentalTerm,
-                        optionInfo.asNumber
+                        rental.rentalEndDate
                 ))
                 .from(rental)
                 .join(rental.productEntity, product)
-                .join(rental.rentalOptionInfoEntity, optionInfo)
+                .join(rental.ownerInfoEntity, ownerInfo)
+                .join(rental.rentalOptionInfoEntity, rentalOptionInfo)
+                .join(rental.deliveryAddressEntity, deliveryAddress)
+                .join(rental.memberEntity, member)
                 .where(whereCondition)
                 .offset(pageable.getOffset()) // 페이지 오프셋 설정
                 .limit(pageable.getPageSize()); // 페이지 크기 설정
@@ -99,7 +121,10 @@ public class OwnerRentalRepositoryCustomImpl implements OwnerRentalRepositoryCus
                 .select(rental.count())
                 .from(rental)
                 .join(rental.productEntity, product)
-                .join(rental.rentalOptionInfoEntity, optionInfo)
+                .join(rental.ownerInfoEntity, ownerInfo)
+                .join(rental.rentalOptionInfoEntity, rentalOptionInfo)
+                .join(rental.deliveryAddressEntity, deliveryAddress)
+                .join(rental.memberEntity, member)
                 .where(whereCondition);
 
         Long countResult = countQuery.fetchOne();
